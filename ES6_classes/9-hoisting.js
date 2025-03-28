02f2822ed2274c18d851f5da833b315e1a5a5854
Crystal-holberton <9188@holbertonstudents.com>
2025-03-28 16:47:06 +1100

    retry

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -28,11 +28,7 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return `${this.holbertonClass}`;
-  }
-
-  get fullStudentDescription() {
-    return `${self._firstName} ${self._lastName} - ${self._holbertonClass.year} - ${self._holbertonClass.location}`;
+    return this.holbertonClass;
   }
 }
 
@@ -43,5 +39,4 @@ const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 const listOfStudents = [student1, student2, student3, student4, student5];
-
 export default listOfStudents;
