b47f3bd39ff0d5fe7df81b5e3cf6620cf02dbaae
Crystal-holberton <9188@holbertonstudents.com>
2025-04-29 14:44:32 +1000

    changed 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """Contains asynchronous coroutine wait_n"""
 import asyncio
-import typing
-from bisect import insort
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> typing.List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Executes multiple coroutines at the same time
     and returns a list of execution times in ascending order.
     
@@ -18,5 +17,5 @@ async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     delays = []
     for task in asyncio.as_completed(tasks):
         delay = await task
-        insort(delays, delay)
+        delays.append(delay)
     return delays
