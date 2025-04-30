b0f72a4ccce5fa78840c93f3ff7d5f1b9ccee946
Crystal-holberton <9188@holbertonstudents.com>
2025-04-30 11:13:41 +1000

    operator space

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -9,6 +9,6 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     a start index and an end index corresponding to the range
     of indexes to return in a list for those particular pagination
     parameters."""
-    start = (page -1) * page_size
+    start = (page - 1) * page_size
     end = page * page_size
     return (start, end)
