f2c6a7c42f88a0c455f999c71b2cf9fd25885855
Crystal-holberton <9188@holbertonstudents.com>
2025-05-19 14:32:29 +1000

    stdin rewrite

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
-// Create a program named 1-stdin.js that will be executed through command line
+// Create a program named 1-stdin.js that will be executed through command line:
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+  process.stdin.end();
 });
 
-// Handle when the input stream ends (e.g., piped input or Ctrl+D)
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
