6c39cca564310bdd897066d0b3aae79ab00c286b
Crystal-holberton <9188@holbertonstudents.com>
2025-05-20 10:04:25 +1000

    http express

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+// create a small HTTP server using Express module
+const express = require('express');
+
+const app = express().get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(1245, () => {
+  console.log('Express server listening on port 1245');
+});
+
+module.exports = app;
